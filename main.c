67bf3ac1867d74970c7e2016609f445777394cab
Arkadiusz Bajura <abajurski@gmail.com>
2023-10-10 01:14:08 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    FILE* file;
+    double x;
+    double angleTable[360];
+     file = fopen("numbers.bin", "wb");
+    if (file == NULL) {
+        printf("Error opening file for writing!\n");
+        return 1;
+    }
+    for (int i = 0; i < 360; ++i) {
+        double angle = i*M_PI/180;
+        x = sin(angle);
+        fwrite(&x, sizeof(double), 1, file);
+    }
+    fclose(file);
+
+    file = fopen("numbers.bin", "rb");
+     if (file == NULL) {
+        printf("Error opening file for reading!\n");
+        return 1;
+    }
+    for (int i = 0; i < 360; ++i) {
+        fread(&angleTable[i], sizeof(double), 1,file);
+    }
+    fclose(file);
+
+    for (int i = 0; i < 360; ++i) {
+        printf("%d - %f\n", i, angleTable[i]);
+    }
+    return 0;
+}
